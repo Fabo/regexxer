135aa780ba76c8a20e1a6aceb32be6969d834d4f
daniel_e <daniel_e>
2002-10-16 05:52:12 +0000

    Insert copyright notice and CVS Id tags.

--- src/sharedptr.cc
+++ src/sharedptr.cc
@@ -1,3 +1,22 @@
+/* $Id$
+ *
+ * Copyright (c) 2002  Daniel Elstner  <daniel.elstner@gmx.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License VERSION 2 as
+ * published by the Free Software Foundation.  You are not allowed to
+ * use any other version of the license; unless you got the explicit
+ * permission from the author to do so.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include <glib.h>
 #include "sharedptr.h"
