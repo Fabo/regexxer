ef6cad3d3748ec5b07c0c54b21902ef8a24c27ce
daniel_e <daniel_e>
2002-11-15 10:28:06 +0000

    Remove the namespace prefix from the friend declaration.  This will hopefully fix a compilation problem with GCC 2.95.x -- once again.

--- src/sharedptr.h
+++ src/sharedptr.h
@@ -44,7 +44,7 @@ private:
   SharedObject(const SharedObject&);
   SharedObject& operator=(const SharedObject&);
 
-  template <class> friend class Util::SharedPtr;
+  template <class> friend class SharedPtr;
 };
 
 
