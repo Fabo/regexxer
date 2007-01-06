7335f7fbb2d00f1aa601709d798266c40c800356
Daniel Elstner <daniel@src.gnome.org>
2007-01-06 04:14:18 +0000

    Update copyright notice: The postal address of the Free Software
    * src/*.{cc,h}: Update copyright notice:  The postal address of
    the Free Software Foundation changed, and my email address too.
    Also, remove the statement I had inserted which explicitely
    disallowed using a later version of the license.  I think I do
    not have to ask for permission from contributors, since the
    copyright notice only mentions me as the copyright holder, so
    technically everyone automatically transferred her copyright
    to me.  Anyway, the change is utterly minor and the notice now
    matches what everyone else is using.
    Also use the opportunity to get rid of the version control
    substitutions and the trailing empty line in every file.
    * ui/aboutdialog.glade: Update my email address.
    
    svn path=/trunk/; revision=589

--- src/filebufferundo.cc
+++ src/filebufferundo.cc
@@ -1,21 +1,21 @@
-/* $Id$
+/*
+ * Copyright (c) 2002-2007  Daniel Elstner  <daniel.kitta@gmail.com>
  *
- * Copyright (c) 2002  Daniel Elstner  <daniel.elstner@gmx.net>
+ * This file is part of regexxer.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License VERSION 2 as
- * published by the Free Software Foundation.  You are not allowed to
- * use any other version of the license; unless you got the explicit
- * permission from the author to do so.
+ * regexxer is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * regexxer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with regexxer; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #include "filebufferundo.h"
@@ -126,4 +126,3 @@ bool FileBufferActionRemoveMatch::do_undo(const sigc::slot<bool>&)
 }
 
 } // namespace Regexxer
-
