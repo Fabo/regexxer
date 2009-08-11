ef3ec769c0d87b2db99bdcec495b56a134ea75e6
Daniel Elstner <daniel.kitta@gmail.com>
2009-08-11 23:51:31 +0200

    Make autogen.sh exit properly if a command failed
    * autogen.sh: Apparently, sub-shells do not inherit the state of the
    -e option, and even ignore explicit uses of 'set -e' within the scope
    of the sub-shell.  In order to work around this stupid behavior, test
    the exit status explicitly for commands executed in a sub-shell.

--- autogen.sh
+++ autogen.sh
@@ -2,8 +2,7 @@
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 (
-  cd "$srcdir"
-  export AUTOPOINT='intltoolize --automake --copy'
-  autoreconf --force --install
-)
+  cd "$srcdir" &&
+  AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install
+) || exit
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
