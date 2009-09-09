c5b25a5421e59933406f48111fa83cb5387c92d0
Daniel Elstner <danielk@openismus.com>
2009-09-09 05:25:13 +0200

    Bump version and generate ChangeLog on make dist
    * ChangeLog: Insert note saying that the file is now auto-generated.
    * Makefile.am (dist-changelog): New rule to generate the distributed
    ChangeLog file.  Hook up to dist-hook.
    * configure.ac (AC_INIT): Increment version to regexxer 0.10.
    (AC_PREREQ): Require Autoconf >= 2.59.
    (AM_INIT_AUTOMAKE): Require Automake >= 1.9.  Use dist-bzip2 option.
    * NEWS: Start news entry for regexxer 0.10 release.
    * autogen.sh: Pass --verbose option to autoreconf.

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,6 @@ test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 (
   cd "$srcdir" &&
-  AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install
+  AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install --verbose
 ) || exit
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
